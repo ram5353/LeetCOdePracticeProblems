54cdef76101199123b4ab054330eb1d0570e9e12
ram5353 <ramkiran5353@gmail.com>
2020-07-25 13:26:13 +0530

    Check if N and it's double exists or not

---
+++ src/Arrays101/CheckIfNAndDoubleExists.java
@@ -0,0 +1,40 @@
+package Arrays101;
+
+import java.util.Arrays;
+
+public class CheckIfNAndDoubleExists {
+    public static void main(String[] args) {
+
+        Solution5 solution5 = new Solution5();
+        System.out.println(solution5.checkIfExist());
+    }
+
+}
+
+class Solution5 {
+    public boolean checkIfExist() {
+        int[] arr = {0,0};
+        System.out.println(Arrays.toString(arr));
+        int number = 0;
+        for (int i=0;i<arr.length;i++) {
+            int count =0 ;
+            if (arr[i] == 0) {
+                count++;
+                if (count > 1) {
+                    return true;
+                }
+            }
+        }
+        for (int value : arr) {
+            number = value * 2 ;
+            for (int v: arr) {
+                if (number != 0) {
+                    if (v == number) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
