18f95126e2941afc18faae340fcb1d887a51ae6a
ram5353 <ramkiran5353@gmail.com>
2020-09-13 00:07:48 +0530

    changes in SubSets

--- src/SubSets.java
+++ src/SubSets.java
@@ -2,26 +2,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SubSets {
+    public static void main(String[] args) {
+        Sub obj = new Sub();
+        System.out.println(obj.subset(new int[]{1, 2, 3}));
+    }
 }
 
 class Sub {
+    List<List<Integer>> output = new ArrayList();
+    int n;
     public void backtrack(int first, ArrayList<Integer> curr, int[] nums) {
-
-        List<List<Integer>> output = new ArrayList();
-        int n;
         output.add(new ArrayList(curr));
-
         for (int i = first; i < nums.length; i++) {
-            // add i into the current combination
             curr.add(nums[i]);
-            // use next integers to complete the combination
             backtrack(i + 1, curr, nums);
-            // backtrack
             curr.remove(curr.size() - 1);
         }
     }
 
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subset(int[] nums) {
         n = nums.length;
         backtrack(0, new ArrayList<Integer>(), nums);
         return output;
