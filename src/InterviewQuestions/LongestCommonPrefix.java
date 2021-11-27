f0ad802f16f424e4a71967c90b6a3bd281b93410
ram5353 <ramkiran5353@gmail.com>
2021-11-27 18:02:40 -0500

    longest common prefix

---
+++ src/InterviewQuestions/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package InterviewQuestions;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i =1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
