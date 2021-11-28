de1a568feb7d4de24dc08fa6975cd8b56e562c4a
ram5353 <ramkiran5353@gmail.com>
2021-11-28 05:06:18 -0500

    k radius changes

--- src/WeeklyContest269/KRadiusSubArrayAverages.java
+++ src/WeeklyContest269/KRadiusSubArrayAverages.java
@@ -14,9 +14,14 @@ public class KRadiusSubArrayAverages {
         int N = nums.length;
         long[] sum = new long[N];
         sum[0] = nums[0];
-        for(int i = 1; i < N; i++)
-            sum[i] = sum[i-1]+nums[i];
-        System.out.println(Arrays.toString(sum));
-        return null;
+        for(int i = 1; i < N; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+        int[] result = new int[N];
+        Arrays.fill(result, -1);
+        for (int i = k; i < N - k; i++) {
+            result[i] = (int) (sum[i+k] - sum[i-k] + nums[i-k])/ ((2 * k) + 1);
+        }
+        return result;
     }
 }
